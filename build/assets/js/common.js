2c79ff5fee1999ef6d1db0d84cb85e4a0aacaa6b
Artem <avd.qu6@gmail.com>
2020-03-11 15:39:06 +0500

    Сделал адаптив на главной

--- build/assets/js/common.js
+++ build/assets/js/common.js
@@ -18,4 +18,47 @@ $(document).ready(function () {
         ]
     });
 
+    // Слайдер партнёров на главной
+    $('.js--partners-slider').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        dots: false,
+        swipe: false,
+        prevArrow: $('.js--partners-slider__arrow_prev'),
+        nextArrow: $('.js--partners-slider__arrow_next'),
+        responsive: [
+            {
+                breakpoint: 1201,
+                settings: {
+                    slidesToShow: 5
+                }
+            },
+            {
+                breakpoint: 993,
+                settings: {
+                    slidesToShow: 4
+                }
+            },
+            {
+                breakpoint: 769,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 577,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 481,
+                settings: {
+                    slidesToShow: 1,
+                    swipe: true
+                }
+            }
+        ]
+    });
+
 });
