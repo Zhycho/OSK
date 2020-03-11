b9ab72acd54976e7bfad3fbc67d3ca00aa446315
Artem <avd.qu6@gmail.com>
2020-03-11 16:26:07 +0500

    Начал мобильное меню

--- src/js/common.js
+++ src/js/common.js
@@ -61,4 +61,62 @@ $(document).ready(function () {
         ]
     });
 
+    // MMenu
+    let $menu = $("#mobile-burger-menu").mmenu({
+        "navbars": [
+            {
+                "position": "top",
+                "content": [
+                    `<div class="mobile-menu__header">
+                        <div class="mobile-menu__header-contacts">
+                            a.header__mail(href="mailto:osk@osk-company.ru") osk@osk-company.ru
+                            a.header_tel(href="tel:+73422357880") +7 (342) 235-78-80
+                        </div>
+                        <a class="mobile-menu__header-close js--mobile-menu__header-close" href="javascript:;">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path><path d="M0 0h24v24H0z" fill="none"></path></svg>
+                        </a>
+                    </div>`
+                ]
+            }
+        ],
+        "extensions": [
+            "position-front",
+            "position-left",
+            "border-full"
+        ],
+        "navbar": {
+            title: false
+        }
+    });
+
+
+    let $icon = $(".js--burger");
+    let API = $menu.data("mmenu");
+    $(document).on('click','.js--mobile-menu__header-close',function(){
+        API.close();
+    });
+
+    $icon.on("click", function () {
+        API.open();
+    });
+
+
+    API.bind("opened", function () {
+        setTimeout(function () {
+            $icon.addClass("is-active");
+        }, 10);
+        $icon.on("click", function () {
+            API.close();
+        });
+    });
+
+    API.bind("closed", function () {
+        setTimeout(function () {
+            $icon.removeClass("is-active");
+        }, 10);
+        $icon.on("click", function () {
+            API.open();
+        });
+    });
+
 });
